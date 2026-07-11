eb134f25f8a201cea6f945edc1263c3b7a1f6ff3
Roman Karki <romankarki118@gmail.com>
2026-07-11 19:26:46 +0545

    url fixed

--- lib/app_constants.dart
+++ lib/app_constants.dart
@@ -5,5 +5,7 @@ const double kSpacingLg = 16.0;
 const double kSpacingXl = 20.0;
 const double kSpacingXxl = 28.0;
 
-// API Configuration
+// API Configuration http://afno pc ko ip:8000
+
 const String kApiBaseUrl = 'http://192.168.1.68:8000';
+
