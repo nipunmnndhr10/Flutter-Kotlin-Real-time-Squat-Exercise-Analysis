7f70b350cbbf2da832735726af5d42742b585766
Prayan Shresth <shresthaprayan02@gmail.com>
2026-07-22 08:37:18 +0545

    changes

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -7,4 +7,4 @@ const double kSpacingXxl = 28.0;
 
 // API Configuration http://afno pc ko ip:8000
 
-const String kApiBaseUrl = 'http://10.0.2.2:8000';
+const String kApiBaseUrl = 'http://172.20.10.7:8000';
