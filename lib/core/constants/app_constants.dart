6721a7b39265fabf6ee5c0c0e8fd4954c5de75f8
Roman Karki <romankarki118@gmail.com>
2026-07-25 21:18:59 +0545

    Remove seed_workouts.py after database seeding

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -8,7 +8,7 @@ const double kSpacingXxl = 28.0;
 // API Configuration http://afno pc ko ip:8000
 
 // azure
-// const String kApiBaseUrl = 'http://13.70.31.20:8000';
+const String kApiBaseUrl = 'http://13.70.31.20:8000';
 
 // your local pc
-const String kApiBaseUrl = 'http://192.168.1.8:8000';
+//const String kApiBaseUrl = 'http://192.168.1.8:8000';
